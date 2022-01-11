245ec3579c747e229c194ef48a79f2b742568cf9
unknown <jadhavsubhash511@gmail.com>
2022-01-11 12:00:47 +0530

    Assignment4 working fine upto 1 billion

--- 10-Jan-2022/Assignment4cpp.cpp
+++ 10-Jan-2022/Assignment4cpp.cpp
@@ -5,11 +5,12 @@
 
 using namespace std;
 
-void numberToWords(int num){
-	int numArray[100];
+string threeDigitsNumberToWords(int num){
+	int numArray[10];
 	int i=0;
 	int number = num;
 	int flag = 0;
+	string result;
 	
 	while(number>0){
 		numArray[i] = number%10;
@@ -20,23 +21,23 @@ void numberToWords(int num){
 		switch(i){
 			case 1: {
 					switch(numArray[0]){
-						case 1: cout<<"One";
+						case 1: result+="One";
 								break;
-						case 2: cout<<"Two";
+						case 2: result+="Two";
 								break;
-						case 3: cout<<"Three";
+						case 3: result+="Three";
 								break;
-						case 4: cout<<"Four";
+						case 4: result+="Four";
 								break;
-						case 5: cout<<"Five";
+						case 5: result+="Five";
 								break;
-						case 6: cout<<"Six";
+						case 6: result+="Six";
 								break;
-						case 7: cout<<"Seven";
+						case 7: result+="Seven";
 								break;
-						case 8: cout<<"Eight";
+						case 8: result+="Eight";
 								break;
-						case 9: cout<<"Nine";
+						case 9: result+="Nine";
 								break;
 						case 0: break;
 						}		
@@ -47,68 +48,68 @@ void numberToWords(int num){
 						case 1:{
 								flag = 1;
 								switch(numArray[0]){
-								case 1: cout<<"Eleven";
+								case 1: result+="Eleven";
 										break;
-								case 2: cout<<"Twelve";
+								case 2: result+="Twelve";
 										break;
-								case 3: cout<<"Thirteen";
+								case 3: result+="Thirteen";
 										break;
-								case 4: cout<<"Fourteen";
+								case 4: result+="Fourteen";
 										break;
-								case 5: cout<<"Fifteen";
+								case 5: result+="Fifteen";
 										break;
-								case 6: cout<<"Sixteen";
+								case 6: result+="Sixteen";
 										break;
-								case 7: cout<<"Seventeen";
+								case 7: result+="Seventeen";
 										break;
-								case 8: cout<<"Eighteen";
+								case 8: result+="Eighteen";
 										break;
-								case 9: cout<<"Nineteen";
+								case 9: result+="Nineteen";
 										break;
-								case 0: cout<<"Ten";
+								case 0: result+="Ten";
 										break;
 							}	
 	
 							break;
 						}
-						case 2: cout<<"Twenty ";
+						case 2: result+="Twenty ";
 								break;
-						case 3: cout<<"Thirty ";
+						case 3: result+="Thirty ";
 								break;
-						case 4: cout<<"Forty ";
+						case 4: result+="Forty ";
 								break;
-						case 5: cout<<"Fifty ";
+						case 5: result+="Fifty ";
 								break;
-						case 6: cout<<"Sixty ";
+						case 6: result+="Sixty ";
 								break;
-						case 7: cout<<"Seventy ";
+						case 7: result+="Seventy ";
 								break;
-						case 8: cout<<"Eighty ";
+						case 8: result+="Eighty ";
 								break;
-						case 9: cout<<"Ninety ";
+						case 9: result+="Ninety ";
 								break;
 					}
 					break;
 				}
 			case 3: {
 					switch(numArray[2]){
-						case 1: cout<<"One Hundred ";
+						case 1: result+="One Hundred ";
 								break;
-						case 2: cout<<"Two Hundred ";
+						case 2: result+="Two Hundred ";
 								break;
-						case 3: cout<<"Three Hundred ";
+						case 3: result+="Three Hundred ";
 								break;
-						case 4: cout<<"Four Hundred ";
+						case 4: result+="Four Hundred ";
 								break;
-						case 5: cout<<"Five Hundred ";
+						case 5: result+="Five Hundred ";
 								break;
-						case 6: cout<<"Six Hundred ";
+						case 6: result+="Six Hundred ";
 								break;
-						case 7: cout<<"Seven Hundred ";
+						case 7: result+="Seven Hundred ";
 								break;
-						case 8: cout<<"Eight Hundred ";
+						case 8: result+="Eight Hundred ";
 								break;
-						case 9: cout<<"Nine Hundred ";
+						case 9: result+="Nine Hundred ";
 								break;
 					}
 					break;
@@ -120,20 +121,48 @@ void numberToWords(int num){
 		}
 		i--;
 	}
-		
 	
-		
+	return result;	
 }
+
+string numberToWords(long int num){
+	string result;
+	
+	while(num> 0){
+		if(num/1000 <1000){
+		 	if(num/1000 == 0){
+		 		result+=threeDigitsNumberToWords(num);
+			 }else{
+			 	result+=threeDigitsNumberToWords(num/1000)+" Thousand "+threeDigitsNumberToWords(num%1000);
+			 }
+			 break;
+		}else if(num/1000000 < 1000 ){
+			if(num%1000000 == 0){
+				result+=threeDigitsNumberToWords(num/1000000) + " Million ";
+			}else{
+				result+=threeDigitsNumberToWords(num/1000000)+" Million ";
+				num = num%1000000;
+			}
+		}
+	}
+	
+return result;	
+}
+
 int main(){
-	int num;
+	long int num;
 	char choice;
 	
 	do{
-		cout<<"Enter an integer : ";
+		a: cout<<"Enter a positive integer : ";
 		cin>>num;
 		
+		if(num<1){
+			cout<<"Invalid Entry => ";
+			goto a;
+		}
 		cout<<num<<" : ";
-		numberToWords(num);
+		cout<<numberToWords(num);
 		cout<<endl;
 		
 		
