65a1521f415be7cf3a43c04d5edcc534f2b6197a
unknown <jadhavsubhash511@gmail.com>
2022-01-12 12:37:13 +0530

    Assignment4

---
+++ 11-Jan-2022/Assignment4.cpp
@@ -0,0 +1,54 @@
+/* word frequency using maps */
+
+#include<iostream>
+#include<map>
+#include<cstring>
+
+using namespace std;
+
+typedef map<string, int>wordFreq;
+
+int main(){
+	char para[1000];
+	string key;
+	string str;
+	
+	string ch;
+	
+	wordFreq word;
+	
+	cout<<"Enter a paragraph upto 1000 characters:"<<endl;
+	
+	cin.getline(para, 1000);
+	
+	wordFreq :: iterator itr;
+	
+	for(int i=0; i<strlen(para)+1; i++){
+		if(para[i] == ' ' || para[i]== '\0'){
+			key = ch;
+			itr = word.find(key);
+
+      		if (itr != word.end())
+            	itr->second = ++(*itr).second;
+			else
+				word.insert(pair<string, int> (key, 1));
+				
+			ch="";
+		}else{
+			ch+=para[i];
+		}
+	}
+	
+	cout<<"Enter the word : ";
+	cin>>str;
+	
+	itr = word.find(str);
+
+    if (itr != word.end())
+        cout<<"`"<<str<<"`"<<" has occured "<<(*itr).second<<" times.";
+	else{
+		cout<<"`"<<str<<"`"<<" is not present in the given paragraph.";
+	}
+	
+return 0;	
+} 
