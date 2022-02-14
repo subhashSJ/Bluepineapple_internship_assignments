8e767426ad615684a62d61715ef2bd037cc6648b
subhashSJ <jadhavsubhash511@gmail.com>
2022-02-14 17:20:18 +0530

    React Js Assignments

---
+++ Web-Design/ReactJS/project-1/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  const showMessage = () => {
+    setMessage("Hello Wolrd!");
+  };
+
+  const hideMessage = () => {
+    setMessage("");
+  };
+  return (
+    <div className="main">
+      <h1>Welcome to the first react project !</h1>
+
+      <div className="container bg-warning border-radius-4">
+        <div className="div flex-direction-column">
+          <p className="message"> {message}</p>
+        </div>
+
+        <div className="div">
+          <button type="button" class="btn btn-secondary" onClick={showMessage}>
+            Show Message
+          </button>
+          <button type="button" class="btn btn-secondary" onClick={hideMessage}>
+            Hide Message
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
