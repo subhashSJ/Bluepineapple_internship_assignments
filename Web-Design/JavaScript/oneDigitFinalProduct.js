2bfa5b06c0de318fee208bbd43483637abf4014d
subhashSJ <jadhavsubhash511@gmail.com>
2022-02-18 13:18:06 +0530

    JS-Assignment-5

---
+++ Web-Design/JavaScript/oneDigitFinalProduct.js
@@ -0,0 +1,23 @@
+/* Q. Create a function that takes one, two or more numbers as 
+   arguments and adds them together to get a new number. The 
+   function then repeatedly multiplies the digits of the new number 
+   by each other, yielding a new number, until the product is only 1
+   digit long. Return the final product. 
+*/
+
+const getOneDigitFinalProduct = (...parameters) => {
+  var sumResult = parameters.reduce((total, currentValue) => {
+    return total + currentValue;
+  });
+
+  while (sumResult > 9) {
+    let str = sumResult.toString();
+    sumResult = new Array(...str).reduce((total, currentValue) => {
+      return Number(total) * Number(currentValue);
+    });
+  }
+
+  return sumResult;
+};
+
+console.log(getOneDigitFinalProduct(2,3,8,10,4));
