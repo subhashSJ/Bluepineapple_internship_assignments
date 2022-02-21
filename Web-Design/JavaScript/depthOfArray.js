6c2ec016438093060a21f98a31c9ae4352ede0e5
subhashSJ <jadhavsubhash511@gmail.com>
2022-02-21 19:46:16 +0530

    Depth of array Assignment

---
+++ Web-Design/JavaScript/depthOfArray.js
@@ -0,0 +1,22 @@
+/* Q. Given an array, write a function to calculate it's depth. 
+      Assume that a normal array has a depth of 1.
+      example:
+        depth([1, 2, 3, 4]) ➞ 1
+        depth([1, [2, 3, 4]]) ➞ 2
+        depth([1, [2, [3, 4]]]) ➞ 3
+        depth([1, [2, [3, [4]]]]) ➞ 4 
+*/
+
+const depth = (arr) => {
+  var depthOfEachElement = [];
+  arr.forEach((element) => {
+    let count = 1;
+    if (Array.isArray(element)) {
+      count += depth(element);
+    }
+    depthOfEachElement.push(count);
+  });
+  return Math.max(...depthOfEachElement);
+};
+
+console.log("Depth : ", depth([1, [2, [3, [4]]], [2, 3], [1, [2, [3, [4]]]]]));
