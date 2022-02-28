ea3a38ee150f1e1ece4453e175210d9225cf76a2
subhashSJ <jadhavsubhash511@gmail.com>
2022-02-28 21:39:18 +0530

    functional programming

---
+++ Functional Programming/c1.js
@@ -0,0 +1,34 @@
+//Currying
+
+//26 Dec 2021 10:55:25:1214Z : DEBUG : this is a debug message
+
+const log = (date, importance, message) =>
+  console.log(
+    `[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`
+  );
+
+const trace = (label) => (value) => {
+  console.log(`${label}: ${value}`);
+  return value;
+};
+
+const curry = (func) => {
+  return function curried(...args) {
+    console.log("args : ", ...args);
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
+};
+
+let curriedlog = curry(log);
+let l_date = curriedlog(new Date()); //curried with first param = date
+let l_info = l_date("INFO"); //curried with second param as well
+let l_debug = l_date("DEBUG"); // ..
+
+l_info("Some Info Message"); //actual invocation
+l_debug("Some Debug Message");
